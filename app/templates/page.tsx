53383447fd2fa72e44d2b0c27b0e28c7147dd7f6
masdjedibelal-dotcom <masdjedibelal@gmail.com>
2026-03-22 23:47:06 +0100

    Delete app directory

--- app/templates/page.tsx
+++
@@ -1,6 +0,0 @@
-import { permanentRedirect } from "next/navigation";
-
-/** Öffentliche Vorlagen-Galerie — nicht verlinkt; Nutzer sollen nur die FlowLeads-Startseite nutzen. */
-export default function TemplatesPage() {
-  permanentRedirect("/");
-}
